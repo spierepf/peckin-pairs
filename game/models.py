eee0727b09c14a8ee92f382bdecf9d647dd6bdda
Peter-Frank Spierenburg <spierepf@hotmail.com>
2026-08-02 18:41:59 -0300

    A GameRound includes a list of eleven distinct image URLs

--- game/models.py
+++ game/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import URLValidator
 from django.db import models
+from django.core.exceptions import ValidationError
+
+import random
 
 # Create your models here.
+validate_url = URLValidator()
+
+
+def validate_image_urls(value):
+    if not isinstance(value, list):
+        raise ValidationError("This field must contain a list.")
+    if len(value) != 11:
+        raise ValidationError("This field must contain exactly eleven entries.")
+    for entry in value:
+        validate_url(entry)
+    if len(set(value)) != len(value):
+        raise ValidationError("This field must contain distinct entries.")
+
+
+def default_image_urls():
+    return [f"http://picsum.photo/seed/{i}/500" for i in random.sample(range(1, 100001), 11)]
+
+
+class GameRound(models.Model):
+    image_urls = models.JSONField(default=default_image_urls, validators=[validate_image_urls])
--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from .models import GameRound
+
+
 # Create your tests here.
+class GameRoundModelTestCase(TestCase):
+    def test_each_game_round_image_urls_must_be_a_list_of_eleven_distinct_urls(self):
+        GameRound.objects.create(image_urls=[f"http://picsum.photo/seed/{i}/500" for i in range(1, 12)]).full_clean()
+
+        # dicts need not apply
+        with self.assertRaises(ValidationError):
+            GameRound.objects.create(
+                image_urls={i: f"http://picsum.photo/seed/{i}/500" for i in range(1, 12)}).full_clean()
+
+        # twelve thou shalt not count
+        with self.assertRaises(ValidationError):
+            GameRound.objects.create(
+                image_urls=[f"http://picsum.photo/seed/{i}/500" for i in range(1, 13)]).full_clean()
+
+        # neither count thou ten, excepting that thou then proceed to eleven
+        with self.assertRaises(ValidationError):
+            GameRound.objects.create(
+                image_urls=[f"http://picsum.photo/seed/{i}/500" for i in range(1, 11)]).full_clean()
+
+        # the image urls must be distinct
+        with self.assertRaises(ValidationError):
+            GameRound.objects.create(
+                image_urls=[f"http://picsum.photo/seed/1/500" for i in range(1, 12)]).full_clean()
+
+    def test_default_game_rounds_are_valid(self):
+        GameRound.objects.create().full_clean()
